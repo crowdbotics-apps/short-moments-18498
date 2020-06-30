1f966b9d1da16b5dd244e9a645cf64b60e9d48ee
Crowdbotics <team@crowdbotics.com>
2020-06-30 22:58:12 +0000

    Update models

--- backend/short_moments_18498/wsgi.py
+++ backend/short_moments_18498/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'short_moments_18498.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "short_moments_18498.settings")
 
 application = get_wsgi_application()
